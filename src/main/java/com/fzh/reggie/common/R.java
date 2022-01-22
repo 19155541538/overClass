1515580916dcae4e394dd8abff18954077f5504a
fzh <616695414@qq.com>
2022-01-22 19:59:54 +0800

    瑞吉外卖优化Day03提交

--- src/main/java/com/fzh/reggie/common/R.java
+++ src/main/java/com/fzh/reggie/common/R.java
@@ -1,5 +1,7 @@
 package com.fzh.reggie.common;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.HashMap;
@@ -8,14 +10,18 @@ import java.util.Map;
 //common 这个包就用来存放 通用的类
 //R是通用返回结果类 服务端响应的数据都会封装成此对象
 @Data
+@ApiModel("返回结果")
 public class R<T> {
-
+    @ApiModelProperty("编码")
     private Integer code; //编码：1成功，0和其它数字为失败
 
+    @ApiModelProperty("错误信息")
     private String msg; //错误信息
 
+    @ApiModelProperty("数据")
     private T data; //数据
 
+    @ApiModelProperty("动态数据")
     private Map map = new HashMap(); //动态数据  用的比较少
 
     //响应成功的方法
--- src/main/java/com/fzh/reggie/controller/SetmealController.java
+++ src/main/java/com/fzh/reggie/controller/SetmealController.java
@@ -10,9 +10,14 @@ import com.fzh.reggie.entity.SetmealDish;
 import com.fzh.reggie.service.CategoryService;
 import com.fzh.reggie.service.SetmealDishService;
 import com.fzh.reggie.service.SetmealService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -28,6 +33,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/setmeal")
 @Slf4j
+@Api(tags = "套餐相关接口")
 public class SetmealController {
 
     @Autowired
@@ -46,6 +52,8 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(value = "setmealCanche",allEntries = true)
+    @ApiOperation(value = "新增套餐接口")
     public R<String> setmealSave(@RequestBody SetmealDto setmealDto) {
         log.info("套餐信息{}", setmealDto);
         setmealService.saveWithDish(setmealDto);
@@ -61,6 +69,12 @@ public class SetmealController {
      * @return
      */
     @GetMapping("/page")
+    @ApiOperation(value = "套餐分页查询接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page",value = "页码",required = true),
+            @ApiImplicitParam(name = "pageSize",value = "每页记录数",required = true),
+            @ApiImplicitParam(name = "page",value = "套餐名称",required = false)
+    })
     public R<Page> page(int page, int pageSize, String name) {
         //分页构造器对象
         Page<Setmeal> pageInfo = new Page<>(page, pageSize);
@@ -130,6 +144,8 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
+    @ApiOperation(value = "套餐删除接口")
     public R<String> delete(@RequestParam List<Long> ids) {
         log.info("ids,{}", ids);
         setmealService.removeWithDish(ids);
